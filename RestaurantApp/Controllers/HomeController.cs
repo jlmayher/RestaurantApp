02af14858af6e092ec123bd61dee01daee30a957
Janet Mayher <mayherjanet@gmail.com>
2019-02-22 11:31:27 -0600

    Add project files.

---
+++ RestaurantApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantApp.Models;
+
+namespace RestaurantApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private static List<Restaurant> restaurants = new List<Restaurant>();
+
+		public ActionResult Index()
+		{
+			ViewBag.restaurants = restaurants;
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Add(string name, string streetaddress, string city, string state, int zipcode)
+		{
+			var newrestaurant = new Restaurant(name, streetaddress, city, state, zipcode);
+			restaurants.Add(newrestaurant);
+			return Redirect("/");
+		}
+
+		public ActionResult Random()
+		{
+			Random randNum = new System.Random(restaurants.Count);
+
+			Restaurant objRest;
+
+			objRest = restaurants[randNum.Next(restaurants.Count)];
+
+			ViewBag.objRest = objRest;
+
+			return View();
+
+		}
+	}
+}
